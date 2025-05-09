b84ab6ac55683d736cb49c1a7ada4011e6379e94
Jonas Lobato <jonaslobato50@gmail.com>
2025-05-08 23:08:47 -0300

    só no back

---
+++ src/lib/db/product.js
@@ -0,0 +1,15 @@
+let playstation = {
+    image:{
+        src:"/images/product/",
+        name:"product-placeholder",
+        alt:"console Playstation 5"
+    },
+    title:"Playstation",
+    price:{
+        current:3200,
+        promotional:2600,
+        installments:6,
+    },
+    description:"Só roda remake, apanha da microsoft",
+}
+export default (playstation); 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script>
 	import Collection from '$lib/Components/Collection/Collection.svelte';
 	import Header from '$lib/Sections/Header/Header.svelte';
+	import playstation from '$lib/db/product';
+
 </script>
 
 <Header />
+<div class="text-white">
+	<img src="{playstation.image.src}{playstation.image.name}.jpg" alt="{playstation.image.alt}">
+	<h2>{playstation.title}</h2>
+	<div>
+		<p class="line-through opacity-70">{playstation.price.current.toLocaleString("pt-BR", {style:"currency", currency: "BRL" })}</p>
+		<p class="text-green-400">{playstation.price.promotional.toLocaleString("pt-BR", {style:"currency", currency: "BRL" })}</p>
+		<p>ou {playstation.price.installments + "x"} de {(playstation.price.promotional / playstation.price.installments).toLocaleString("pt-BR", {style:"currency", currency: "BRL" })}</p>
+	</div>
+	<p>{playstation.description}</p>
+</div>
 <Collection collectionName="Consoles"/>
