0a2189bd14dd32713b803c69c778c881310dcc07
Jonas Lobato <jonaslobato50@gmail.com>
2025-05-13 18:15:28 -0300

    add product db

--- src/lib/db/products.js
+++ src/lib/db/products.js
@@ -12,20 +12,6 @@ let playstation = [
 			installments: 6
 		},
 		description: 'Só roda remake, apanha da microsoft'
-	},
-	{
-		image: {
-			src: '/images/product/',
-			name: 'product-placeholder',
-			alt: 'console Playstation 5'
-		},
-		title: 'Playstation',
-		price: {
-			current: 3200,
-			promotional: 2600,
-			installments: 6
-		},
-		description: 'Só roda remake, apanha da microsoft'
 	}
 ];
 let xbox = [
@@ -33,15 +19,69 @@ let xbox = [
 		image: {
 			src: '/images/product/',
 			name: 'product-placeholder-01',
-			alt: 'console Playstation 5'
+			alt: 'console Xbox Series S'
 		},
 		title: 'Xbox Series S',
 		price: {
-			current: 3200,
-			promotional: 2600,
+			current: 2600,
+			promotional: 2499.99,
 			installments: 6
 		},
 		description: 'Da microsoft'
+	},
+	{
+		image: {
+			src: '/images/product/',
+			name: 'product-placeholder-03',
+			alt: 'Red Dead Redemption 2 - Xbox One / XS - Mídia Digital'
+		},
+		title: 'Red Dead Redemption 2 - Xbox One / XS - Mídia Digital',
+		price: {
+			current: 169.90,
+			promotional: 49.90,
+			installments: 6
+		},
+		description: 'Inclui Red Dead Redemption 2: Modo História e Red Dead Online.'
+	},
+	{
+		image: {
+			src: '/images/product/',
+			name: 'product-placeholder-04',
+			alt: 'EA Sports FC 25 – Xbox One / XS – Mídia Digital'
+		},
+		title: 'EA Sports FC 25 – Xbox One / XS – Mídia Digital',
+		price: {
+			current: 199.99,
+			promotional: 59.90,
+			installments: 6
+		},
+		description: 'King of football.'
+	},{
+		image: {
+			src: '/images/product/',
+			name: 'product-placeholder-05',
+			alt: 'Resident Evil 4 Remake – Xbox Series XS – Mídia Digital'
+		},
+		title: 'Resident Evil 4 Remake – Xbox Series XS – Mídia Digital',
+		price: {
+			current: 143.99,
+			promotional: 72.90,
+			installments: 6
+		},
+		description: 'Experimente o terror renovado em Resident Evil 4.'
+	},{
+		image: {
+			src: '/images/product/',
+			name: 'product-placeholder-06',
+			alt: 'Red Dead Redemption 2 - Xbox One / XS - Mídia Digital'
+		},
+		title: 'GTA 5 Grand Theft Auto V: Edição Premium – Xbox One / XS – Mídia Digital',
+		price: {
+			current: 89.90,
+			promotional: 39.90,
+			installments: 6
+		},
+		description: 'GRAND THEFT AUTO V: EDIÇÃO PREMIUM.'
 	},
 	
 ];
@@ -50,12 +90,12 @@ let nintendo = [
 		image: {
 			src: '/images/product/',
 			name: 'product-placeholder-02',
-			alt: 'console Playstation 5'
+			alt: 'Nintendo Switch 2'
 		},
-		title: 'Nintendo',
+		title: 'Nintendo Switch 2',
 		price: {
-			current: 3200,
-			promotional: 2600,
+			current: 4499.90,
+			promotional: 4499.90,
 			installments: 6
 		},
 		description: 'Só tem Mário'
